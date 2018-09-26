acb3561ec4f1edb820be16915b46541cf6c47500
Julien Breiner <julien@dwd.com.au>
2018-09-26 19:55:15 +0200

    initialize the game

--- src/front/main.ts
+++ src/front/main.ts
@@ -11,12 +11,18 @@ if (document.getElementById("game")) {
     initGame();
 }
 
+/**
+ * Initialize the game
+ */
 async function initGame() {
+    // Get the game id
     const res = await Axios.get("/api/gameId");
 
+    // Something went wrong. reload the page to go to login
     if (res.data.success === false) {
         return window.location.reload();
     }
 
+    // Start the game
     game = new Game(res.data.gid, res.data.uid);
 }
